263b1e33efea51165a92b23ecc421e822153b185
Jake Price <hello@jpcreativemedia.ca>
2026-02-06 17:36:12 -0800

    updated fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import Foooter from "@/ui/layout/Footer";
 import Header from "@/ui/layout/Header";
@@ -16,6 +17,26 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const urbanist = localFont({
+  src: [
+    {
+      path: "../fonts/urbanist/Urbanist-VariableFont_wght.ttf",
+      style: "normal",
+    },
+    {
+      path: "../fonts/urbanist/Urbanist-Italic-VariableFont_wght.ttf",
+      style: "italic",
+    },
+  ],
+  display: "swap",
+  variable: "--font-urbanist",
+});
+
 export const metadata: Metadata = {
   title: "MVP - Markdown",
   description: "",
@@ -28,14 +49,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="min-h-full">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-full`}
-      >
+      <body className={`${inter.className} antialiased min-h-full`}>
         <Header />
         {/*
         <HeaderHover />
          */}
-        <div className="flex flex-col min-h-screen items-center justify-center bg-white font-sans dark:bg-black">
+        <div className="flex flex-col min-h-screen items-center justify-center bg-white font-sans overflow-x-clip ">
           {children}
         </div>
         <Foooter />
